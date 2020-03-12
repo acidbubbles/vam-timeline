7c01460eaffec19c3f360261502e329f7cac60d6
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-03-11 22:22:24 -0400

    Mark selection begin/end

--- src/Atoms/UI/VamTimeline.AtomAnimationBulkUI.cs
+++ src/Atoms/UI/VamTimeline.AtomAnimationBulkUI.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+
 namespace VamTimeline
 {
     /// <summary>
@@ -10,6 +13,9 @@ namespace VamTimeline
     {
         public const string ScreenName = "Bulk Operations";
         public override string Name => ScreenName;
+        private JSONStorableString _selectionJSON;
+        private float _selectionStart = 0;
+        private float _selectionEnd = 0;
 
         public AtomAnimationBulkUI(IAtomPlugin plugin)
             : base(plugin)
@@ -27,6 +33,77 @@ namespace VamTimeline
             InitPlaybackUI(false);
 
             InitFrameNavUI(false);
+
+            InitSelectionUI();
+        }
+
+        private void InitSelectionUI()
+        {
+            _selectionJSON = new JSONStorableString("Selected Frames", "")
+            {
+                isStorable = false
+            };
+            Plugin.CreateTextField(_selectionJSON, true);
+            _linkedStorables.Add(_selectionJSON);
+
+            var markSelectionStartUI = Plugin.CreateButton("Mark Selection Start", true);
+            markSelectionStartUI.button.onClick.AddListener(MarkSelectionStart);
+            _components.Add(markSelectionStartUI);
+
+            var markSelectionEndUI = Plugin.CreateButton("Mark Selection End", true);
+            markSelectionEndUI.button.onClick.AddListener(MarkSelectionEnd);
+            _components.Add(markSelectionEndUI);
+
+            _selectionStart = 0f;
+            _selectionEnd = Plugin.Animation.Current.AnimationLength;
+            SelectionModified();
+        }
+
+        private void MarkSelectionStart()
+        {
+            _selectionStart = Plugin.Animation.Time;
+            if (_selectionEnd < _selectionStart) _selectionEnd = _selectionStart;
+            SelectionModified();
+        }
+
+        private void MarkSelectionEnd()
+        {
+            _selectionEnd = Plugin.Animation.Time;
+            if (_selectionStart > _selectionEnd) _selectionStart = _selectionEnd;
+            SelectionModified();
+        }
+
+        private void SelectionModified()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Selected {_selectionStart:0.000}-{_selectionEnd:0.000}s of {Plugin.Animation.Current.AnimationLength:0.000}s");
+            var involvedKeyframes = 0;
+            foreach (var target in Plugin.Animation.Current.GetAllOrSelectedTargets())
+            {
+                var leadCurve = target.GetLeadCurve();
+                for (var key = 0; key < leadCurve.length; key++)
+                {
+                    var keyTime = leadCurve[key].time;
+                    if (keyTime >= _selectionStart && keyTime <= _selectionEnd)
+                        involvedKeyframes++;
+                }
+                if (involvedKeyframes > 0)
+                    sb.AppendLine($"- {target.Name}: {involvedKeyframes} keyframes");
+            }
+            _selectionJSON.val = sb.ToString();
+        }
+
+        public override void AnimationModified()
+        {
+            base.AnimationModified();
+
+            var current = Plugin.Animation.Current;
+            if (current.AnimationLength < _selectionEnd)
+            {
+                _selectionEnd = current.AnimationLength;
+                if (_selectionStart > _selectionEnd) _selectionStart = _selectionEnd;
+                SelectionModified();
+            }
         }
     }
 }
