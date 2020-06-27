3a5cfcc809f4d004aef3ef5b72014bddda89e291
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-06-27 14:27:33 -0400

    Auto-generate all files in cslist, csproj and meta.json files

--- Update-FilesLists.ps1
+++ Update-FilesLists.ps1
@@ -1,7 +1,34 @@
 cd (Split-Path ($MyInvocation.MyCommand.Path))
 
-( `
-    ls src/*.cs -Recurse `
+# Get Atom animation files
+$atomAnimationFiles = ( `
+    ls ./src/*.cs -Recurse `
     | ? { -not $_.FullName.Contains("Controller\") -and $_.Name -ne "ControllerPlugin.cs" } `
     | % { $_.FullName.Substring((pwd).Path.Length + 1) } `
-) > .\VamTimeline.AtomAnimation.cslist
+)
+
+# VamTimeline.AtomAnimation.cslist
+$atomAnimationFiles > .\VamTimeline.AtomAnimation.cslist
+
+# VamTimeline.csproj
+( Get-Content ".\VamTimeline.csproj" -Raw ) -Replace "(?sm)(?<=^ +<!-- AtomAnimationFiles -->`r?`n).*?(?=`r?`n +<!-- /AtomAnimationFiles -->)", `
+    [System.String]::Join("`r`n", ($atomAnimationFiles | % { "    <Compile Include=`"$_`" />" } ) ) | `
+    Set-Content ".\VamTimeline.csproj"
+
+# meta.json
+$allFiles = (ls ./src/*.cs -Recurse) + (ls *.cslist) `
+    | % { $_.FullName.Substring((pwd).Path.Length + 1) }
+( Get-Content ".\meta.json" -Raw ) -Replace "(?sm)(?<=^  `"contentList`": \[`r?`n).*?(?=`r?`n  \],)", `
+    [System.String]::Join("`r`n", ($allFiles | % { "    `"Custom\\Scripts\\AcidBubbles\\Timeline\\$($_.Replace("\", "\\"))`"," } ) ).Trim(",") `
+| Set-Content ".\meta.json"
+
+# tests/VamTimeline.Tests.cslist
+$testFiles = ( `
+    $atomAnimationFiles `
+    | ? { -not $_.EndsWith("\AtomPlugin.cs") } `
+    | % { "..\$_" } `
+) + ( `
+    ls ./tests/*.cs -Recurse `
+    |  % { $_.FullName.Substring((pwd).Path.Length + 7) } `
+)
+$testFiles > .\tests\VamTimeline.Tests.cslist
