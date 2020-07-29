604fe0d3f0965f183064097228e9648cd0703315
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-07-28 22:40:50 -0400

    Fix unstable curve when the value is very near zero with multiple frames

--- src/UI/Components/Curves/CurvesLines.cs
+++ src/UI/Components/Curves/CurvesLines.cs
@@ -45,7 +45,7 @@ namespace VamTimeline
             var maxY = range.y;
             var yRatio = height / (maxY - minY);
             var offsetY = (-minY - (maxY - minY) / 2f) * yRatio;
-            if (float.IsInfinity(yRatio))
+            if (float.IsInfinity(yRatio) || maxY - minY <= float.Epsilon)
             {
                 yRatio = 1f;
                 offsetY = 0f;
