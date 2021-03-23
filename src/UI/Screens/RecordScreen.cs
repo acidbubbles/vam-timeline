7c5c68e2adc32a82aec0221147c4cdc20ad2e145
Acid Bubbles <acidbubbles.anon@gmail.com>
2021-03-22 21:05:45 -0400

    Do not allow recording unless something is selected

--- src/UI/Screens/RecordScreen.cs
+++ src/UI/Screens/RecordScreen.cs
@@ -5,6 +5,7 @@ namespace VamTimeline
 {
     public class RecordScreen : ScreenBase
     {
+        private UIDynamicButton _recordButton;
         public const string ScreenName = "Record";
 
         public override string screenId => ScreenName;
@@ -16,7 +17,22 @@ namespace VamTimeline
 
             CreateChangeScreenButton("<b><</b> <i>Back</i>", MoreScreen.ScreenName);
 
-            prefabFactory.CreateButton("Start recording in 1, 2, 3...").button.onClick.AddListener(() => plugin.StartCoroutine(OnRecordCo()));
+            _recordButton = prefabFactory.CreateButton("Start recording in 1, 2, 3...");
+            _recordButton.button.onClick.AddListener(() => plugin.StartCoroutine(OnRecordCo()));
+
+            animationEditContext.onTargetsSelectionChanged.AddListener(OnTargetsSelectionChanged);
+            OnTargetsSelectionChanged();
+        }
+
+        private void OnTargetsSelectionChanged()
+        {
+            _recordButton.button.interactable = animationEditContext.GetSelectedTargets().Any();
+        }
+
+        public override void OnDestroy()
+        {
+            animationEditContext.onTargetsSelectionChanged.RemoveListener(OnTargetsSelectionChanged);
+            base.OnDestroy();
         }
 
         private IEnumerator OnRecordCo()
