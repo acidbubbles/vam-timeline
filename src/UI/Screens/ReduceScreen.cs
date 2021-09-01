eb6696b9cf16cfa247d1c50ec7a114e6a72b9899
Acid Bubbles <acidbubbles.anon@gmail.com>
2021-08-31 23:41:21 -0400

    Fix crash in reduce screen when loading it twice

--- src/UI/Screens/ReduceScreen.cs
+++ src/UI/Screens/ReduceScreen.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -8,31 +7,19 @@ namespace VamTimeline
     {
         public const string ScreenName = "Reduce";
 
-        private static readonly JSONStorableFloat _reduceMaxFramesPerSecondJSON;
-        private static readonly JSONStorableBool _averageToSnapJSON;
-        private static readonly JSONStorableBool _removeFlatSectionsKeyframes;
-        private static readonly JSONStorableBool _simplifyKeyframes;
-        private static readonly JSONStorableFloat _reduceMinDistanceJSON;
-        private static readonly JSONStorableFloat _reduceMinRotationJSON;
-        private static readonly JSONStorableFloat _reduceMinFloatParamRangeRatioJSON;
+        private JSONStorableFloat _reduceMaxFramesPerSecondJSON;
+        private JSONStorableBool _averageToSnapJSON;
+        private JSONStorableBool _removeFlatSectionsKeyframes;
+        private JSONStorableBool _simplifyKeyframes;
+        private JSONStorableFloat _reduceMinDistanceJSON;
+        private JSONStorableFloat _reduceMinRotationJSON;
+        private JSONStorableFloat _reduceMinFloatParamRangeRatioJSON;
 
         public override string screenId => ScreenName;
 
         private UIDynamicButton _backupUI;
         private UIDynamicButton _restoreUI;
         private UIDynamicButton _reduceUI;
-
-        static ReduceScreen()
-        {
-            _reduceMaxFramesPerSecondJSON = new JSONStorableFloat("Frames per second", 25f, 1f, 100f);
-            _reduceMaxFramesPerSecondJSON.setCallbackFunction = val => _reduceMaxFramesPerSecondJSON.valNoCallback = Mathf.Round(val);
-            _averageToSnapJSON = new JSONStorableBool("Average and snap to fps", true);
-            _removeFlatSectionsKeyframes = new JSONStorableBool("Remove flat sections", true);
-            _simplifyKeyframes = new JSONStorableBool("Simplify keyframes", true);
-            _reduceMinDistanceJSON = new JSONStorableFloat("Minimum meaningful distance", 0.1f, 0f, 1f, false);
-            _reduceMinRotationJSON = new JSONStorableFloat("Minimum meaningful rotation (dot)", 0.001f, 0f, 1f);
-            _reduceMinFloatParamRangeRatioJSON = new JSONStorableFloat("Minimum meaningful float range ratio", 0.01f, 0f, 1f);
-        }
 
         public override void Init(IAtomPlugin plugin, object arg)
         {
@@ -48,13 +35,8 @@ namespace VamTimeline
             _restoreUI.button.onClick.AddListener(RestoreBackup);
 
             prefabFactory.CreateSpacer();
-            prefabFactory.CreateSlider(_reduceMaxFramesPerSecondJSON).valueFormat = "F1";
-            prefabFactory.CreateToggle(_averageToSnapJSON);
-            prefabFactory.CreateToggle(_removeFlatSectionsKeyframes);
-            prefabFactory.CreateToggle(_simplifyKeyframes);
-            prefabFactory.CreateSlider(_reduceMinDistanceJSON).valueFormat = "F3";
-            prefabFactory.CreateSlider(_reduceMinRotationJSON).valueFormat = "F4";
-            prefabFactory.CreateSlider(_reduceMinFloatParamRangeRatioJSON).valueFormat = "F3";
+
+            CreateReduceSettingsUI();
 
             _reduceUI = prefabFactory.CreateButton("Reduce");
             _reduceUI.button.onClick.AddListener(Reduce);
@@ -64,6 +46,25 @@ namespace VamTimeline
 
             animationEditContext.animation.animatables.onTargetsSelectionChanged.AddListener(OnTargetsSelectionChanged);
             OnTargetsSelectionChanged();
+        }
+
+        private void CreateReduceSettingsUI()
+        {
+            _reduceMaxFramesPerSecondJSON = new JSONStorableFloat("Frames per second", 25f, 1f, 100f);
+            _reduceMaxFramesPerSecondJSON.setCallbackFunction = val => _reduceMaxFramesPerSecondJSON.valNoCallback = Mathf.Round(val);
+            _averageToSnapJSON = new JSONStorableBool("Average and snap to fps", true);
+            _removeFlatSectionsKeyframes = new JSONStorableBool("Remove flat sections", true);
+            _simplifyKeyframes = new JSONStorableBool("Simplify keyframes", true);
+            _reduceMinDistanceJSON = new JSONStorableFloat("Minimum meaningful distance", 0.1f, 0f, 1f, false);
+            _reduceMinRotationJSON = new JSONStorableFloat("Minimum meaningful rotation (dot)", 0.001f, 0f, 1f);
+            _reduceMinFloatParamRangeRatioJSON = new JSONStorableFloat("Minimum meaningful float range ratio", 0.01f, 0f, 1f);
+            prefabFactory.CreateSlider(_reduceMaxFramesPerSecondJSON).valueFormat = "F1";
+            prefabFactory.CreateToggle(_averageToSnapJSON);
+            prefabFactory.CreateToggle(_removeFlatSectionsKeyframes);
+            prefabFactory.CreateToggle(_simplifyKeyframes);
+            prefabFactory.CreateSlider(_reduceMinDistanceJSON).valueFormat = "F3";
+            prefabFactory.CreateSlider(_reduceMinRotationJSON).valueFormat = "F4";
+            prefabFactory.CreateSlider(_reduceMinFloatParamRangeRatioJSON).valueFormat = "F3";
         }
 
         protected override void OnCurrentAnimationChanged(AtomAnimationEditContext.CurrentAnimationChangedEventArgs args)
