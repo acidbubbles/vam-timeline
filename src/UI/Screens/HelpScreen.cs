2078e76a8327bc680075a9758d76b60d745f2641
Acid Bubbles <acidbubbles.anon@gmail.com>
2022-03-28 10:54:50 -0400

    Fix help text to match the latest interface

--- src/UI/Screens/HelpScreen.cs
+++ src/UI/Screens/HelpScreen.cs
@@ -7,37 +7,37 @@ namespace VamTimeline
         private const string _helpText = @"
 <b>Welcome to Timeline!</b>
 
-This plugin allows for advanced keyframe-based editing.
+This plugin allows creating advanced keyframe-based animations.
 
-Remember that there is documentation available in the wiki (accessible from the More menu), as well as video tutorials.
+There is documentation available in the wiki (accessible from the More menu), as well as video tutorials.
 
 <b>The UI</b>
 
 On your left, you can find the list of animations. By default, there's only one: 'Anim 1'.
 
-Then you have the <i>scrubber</i>. It shows the animation time.
+Under, you have the keyframe navigation controls. The leftmost and righmost buttons go to the next/previous frame, the inner buttons let you move backward and forward, and the center button 'snaps' to the closest second.
 
-Under, you have the frame navigation controls. The leftmost and righmost buttons go to the next/previous frame, the inner buttons let you move backward and forward, and the center button 'snaps' to the closed second.
+There are two Play buttons. ""All"" is used to play all layers at once, and will play sequences. The second (named after the current animation) will only play the current clip.
 
-There are two Play buttons. ""Seq"" is used to play all layers, and play sequences. ""Clip"" will only play the current clip.
+Then you have the <i>scrubber</i>. It shows the animation time and where you currently are. You can move the time back and forth, and zoom in/out.
 
-Then you can see the <i>dope sheet</i>. This shows you all <i>targets</i> (e.g. a hand or a smile morph), and whether there's a keyframe at any point in time.
+Underneath, you have the <i>dope sheet</i>. This shows you all <i>targets</i> (e.g. a hand or a smile morph), and whether there's a keyframe at any point in time. You can switch to the Curves view using the top-left button.
 
 On the top you can find the <i>tabs</i>. This is how you'll navigate to the more advanced functions.
 
 <b>Your first animation</b>
 
-Select the add target button in the Edit panel.
+Select the 'Add/remove targets' button in the Targets panel.
 
 Choose what you want to animate (for example, the right hand controller).
 
 Move the right hand to a new position, move the scrubber to 1s, and move the right hand to another position.
 
-Now, rewind (press stop) and play. You'll see the right hand move back and forth between the two positions. This is because the animation is <i>looping</i>.
+Now, rewind (press stop) and play. You'll see the right hand moves back and forth between the two positions. This is because the animation is <i>looping</i>.
 
 <b>Learning</b>
 
-Check out the wiki for more details and videos. There's a ton of things you can do! Now have fun!
+Check out the wiki (link in the More menu) for more documentation and videos. There's a ton of things you can do! Now have fun!
 ";
 
         public override string screenId => ScreenName;
